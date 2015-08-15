c76ab719912129199755e0f1bc43c3c8ddf43d74
Robin Kanters <robin@robinkanters.nl>
2015-08-15 19:45:56 +0200

    Quiz must have questions when fed into quizmaster

--- src/QuizChallenge.Model/QuizMasterFactory.cs
+++ src/QuizChallenge.Model/QuizMasterFactory.cs
@@ -1,11 +1,16 @@
 ﻿namespace QuizChallenge.Model
 {
+    using System.IO;
+    using System.Linq;
     using ModelInterfaces;
 
     public class QuizMasterFactory
     {
         public static QuizMaster Create(IQuiz chosenQuiz, IGameCommandListener gameCommandListener)
         {
+            if (!chosenQuiz.Questions.Any())
+                throw new InvalidDataException("No questions in this quiz!");
+
             return new QuizMaster(chosenQuiz, gameCommandListener);
         }
     }
