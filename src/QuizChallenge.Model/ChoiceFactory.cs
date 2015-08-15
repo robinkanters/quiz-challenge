09136b5b68905e3d39abc306156015b3586465cf
Robin Kanters <robin@robinkanters.nl>
2015-08-15 09:58:01 +0200

    doing so checks when creating a choice

--- src/QuizChallenge.Model/ChoiceFactory.cs
+++ src/QuizChallenge.Model/ChoiceFactory.cs
@@ -1,9 +1,16 @@
 ﻿namespace QuizChallenge.Model
 {
+    using System.IO;
+
     public class ChoiceFactory
     {
         public static Choice Create(string choiceText, int value)
         {
+            if (choiceText.Length <= 0)
+                throw new InvalidDataException($"{choiceText} is not a valid choice text");
+            if(value < 0)
+                throw new InvalidDataException($"{value} is not a valid value, must be 0 or higher");
+
             return new Choice(choiceText, value);
         }
     }
