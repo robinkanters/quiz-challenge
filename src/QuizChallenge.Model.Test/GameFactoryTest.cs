30cef0de45a66c0f61de06e822674cb0aaab6a56
Robin Kanters <robin@robinkanters.nl>
2015-08-15 20:26:25 +0200

    Added GameFactoryTest unit tests

---
+++ src/QuizChallenge.Model.Test/GameFactoryTest.cs
@@ -0,0 +1,45 @@
+﻿namespace QuizChallenge.Model.Test
+{
+    using System;
+    using NUnit.Framework;
+
+    public class GameFactoryTest
+    {
+        [Test]
+        public void CreateGame_WithValidArguments_SuccessfullyCreatesNewGame()
+        {
+            var quiz = new Quiz("testquiz", new Question("question", new Choice("choice", 10)));
+            var game = GameFactory.Create(quiz);
+
+            Assert.That(game, Is.Not.Null);
+        }
+
+        [Test]
+        public void CreateGame_AfterCreation_StillHasTheQuizName()
+        {
+            const string quizName = "testquiz";
+
+            var quiz = new Quiz(quizName, new Question("question", new Choice("choice", 10)));
+            var game = GameFactory.Create(quiz);
+
+            Assert.That(game.Quiz.Name, Is.EqualTo(quizName));
+        }
+
+        [Test]
+        public void CreateGame_AfterCreation_StillHasTheQuiz()
+        {
+            var quiz = new Quiz("quizname", new Question("", new Choice("", 0)));
+            var game = GameFactory.Create(quiz);
+
+            Assert.That(game.Quiz, Is.EqualTo(quiz));
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void CreateGame_WithNullQuiz_ThrowsException()
+        {
+            var game = GameFactory.Create(null);
+            Assert.That(game, Is.Null);
+        }
+    }
+}
--- src/QuizChallenge.Model/GameFactory.cs
+++ src/QuizChallenge.Model/GameFactory.cs
@@ -1,11 +1,15 @@
 ﻿namespace QuizChallenge.Model
 {
+    using System;
     using ModelInterfaces;
 
     public class GameFactory
     {
         public static Game Create(IQuiz quiz)
         {
+            if(quiz == null)
+                throw new InvalidOperationException("Quiz cannot be null");
+
             return new Game(quiz);
         }
     }
