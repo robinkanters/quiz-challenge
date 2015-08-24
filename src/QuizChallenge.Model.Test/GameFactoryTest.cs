b508cdca5add52503c9cd93f0ad76c978ab767db
Robin Kanters <robin@robinkanters.nl>
2015-08-24 15:02:21 +0200

    Discarded some rediculous tests [ci skip]

--- src/QuizChallenge.Model.Test/GameFactoryTest.cs
+++ src/QuizChallenge.Model.Test/GameFactoryTest.cs
@@ -16,26 +16,6 @@
         }
 
         [Test]
-        public void CreateGame_AfterCreation_StillHasTheQuizName()
-        {
-            const string quizName = "testquiz";
-
-            var quiz = new Quiz(quizName, new Question("question", new Choice("choice", 10)));
-            var game = GameFactory.Create(quiz);
-
-            Assert.That(game.Quiz.Name, Is.EqualTo(quizName));
-        }
-
-        [Test]
-        public void CreateGame_AfterCreation_StillHasTheQuiz()
-        {
-            var quiz = new Quiz("quizname", new Question("", new Choice("", 0)));
-            var game = GameFactory.Create(quiz);
-
-            Assert.That(game.Quiz, Is.EqualTo(quiz));
-        }
-
-        [Test]
         [ExpectedException(typeof(InvalidOperationException))]
         public void CreateGame_WithNullQuiz_ThrowsException()
         {
