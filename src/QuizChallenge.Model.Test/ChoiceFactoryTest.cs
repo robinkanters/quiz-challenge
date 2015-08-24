79d17be2d5dbe1f898ce3a9b4da0808efdea7ba6
Robin Kanters <robin@robinkanters.nl>
2015-08-24 15:09:07 +0200

    Added tests for ChoiceFactory

---
+++ src/QuizChallenge.Model.Test/ChoiceFactoryTest.cs
@@ -0,0 +1,33 @@
+﻿namespace QuizChallenge.Model.Test
+{
+    using System.IO;
+    using NUnit.Framework;
+
+    public class ChoiceFactoryTest
+    {
+        [Test]
+        [TestCase(-1)]
+        [ExpectedException(typeof(InvalidDataException))]
+        public void Create_WithInvalidChoiceValue_ThrowsException(int testValue)
+        {
+            const string choiceText = "text doesn't matter for test";
+
+            var choice = ChoiceFactory.Create(choiceText, testValue);
+
+            Assert.That(choice, Is.Null);
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        public void Create_WithValidChoiceValue_CreatesChoiceObject(int testValue)
+        {
+            const string choiceText = "text doesn't matter for test";
+
+            var choice = ChoiceFactory.Create(choiceText, testValue);
+
+            Assert.That(choice, Is.Not.Null);
+            Assert.That(choice.Value, Is.EqualTo(testValue));
+        }
+    }
+}
