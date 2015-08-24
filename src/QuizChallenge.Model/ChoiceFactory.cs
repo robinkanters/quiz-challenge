61eaefab4c7865f81dbda272baea149020a255a1
Robin Kanters <robinkanters@users.noreply.github.com>
2015-08-24 15:03:25 +0200

    OCD
    [ci skip]

--- src/QuizChallenge.Model/ChoiceFactory.cs
+++ src/QuizChallenge.Model/ChoiceFactory.cs
@@ -8,7 +8,7 @@
         {
             if (choiceText.Length <= 0)
                 throw new InvalidDataException($"{choiceText} is not a valid choice text");
-            if(value < 0)
+            if (value < 0)
                 throw new InvalidDataException($"{value} is not a valid value, must be 0 or higher");
 
             return new Choice(choiceText, value);
